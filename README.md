1faad047960f48281522b734aea8486d238aae23
nsum <your.marketer1303@gmail.com>
2021-02-15 18:57:22 +0000

    Connect Flask to MongoDB

---
+++ README.md
@@ -0,0 +1,4 @@
+### Requirements.txt
+- in order for flask to communicate to mongo we need to install flask-pymongo (similar to pymongo)
+- in order to use mongo connection string we need to install dnspython
+- after adding any libs or packages always update requirements.txt so app knows what to use
--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+# ObjectId is imported because mongo stores data in JSON like
+# format called bson
 if os.path.exists("env.py"):
     import env
 
@@ -7,11 +13,24 @@ if os.path.exists("env.py"):
 # Create instance of flask
 app = Flask(__name__)
 
+# Grab database name
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+# Configure connection string
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# Grab SECRET_KEY
+app.secret_key = os.environ.get("SECRET_KEY")
+# Add instane of pymongo
+# app in brackets is app object we created above (instance of Flask)
+mongo = PyMongo(app)
+
 
 # / refers to default route
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks= tasks)
+    # 1st task is what tamplate will use and that = 2nd tasks we created (var)
 
 
 if __name__ == "__main__":
