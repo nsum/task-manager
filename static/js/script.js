4cf5c79baed1088a9784e4cee8edaf10848b8439
nsum <your.marketer1303@gmail.com>
2021-02-17 00:37:11 +0000

    Add categories dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     // enable urgent task tooltip
     $('.tooltipped').tooltip();
+    // enable category select dropdown in new task creation
+    $('select').formSelect();
     // enable date picker when creating new task
     $('.datepicker').datepicker({
         format: "dd mmmm, yyyy",
