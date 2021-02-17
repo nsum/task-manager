d05e9654cd94c0688cd8fe00baf8b417bba77287
nsum <your.marketer1303@gmail.com>
2021-02-17 00:27:53 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     // enable urgent task tooltip
     $('.tooltipped').tooltip();
+    // enable date picker when creating new task
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
   });
